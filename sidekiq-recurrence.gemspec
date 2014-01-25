159171ac60634e88398732888dd23892395b1efa
Adam Farhi <afarhi@ebay.com>
2014-01-25 14:11:17 +0200

    testing out ice_cube time conversations

--- sidekiq-recurrence.gemspec
+++ sidekiq-recurrence.gemspec
@@ -20,7 +20,13 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "timecop"
+  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "spork-rails" #, :github => 'sporkrb/spork-rails'
+  spec.add_development_dependency "guard-rspec"
+  spec.add_development_dependency "guard-spork"
 
-  gem.add_dependency "sidekiq", ">= 2.17.2"
-  gem.add_dependency "whenever", ">= 0.9.0"
+  spec.add_dependency "sidekiq", ">= 2.17.2"
+  spec.add_dependency "whenever", ">= 0.9.0"
+  spec.add_dependency "ice_cube", ">= 0.11.1"
 end
---
+++ spec/lib/times_spec.rb
@@ -0,0 +1,89 @@
+require 'rspec'
+require 'whenever'
+require 'ice_cube'
+
+class TimeConversions
+
+  DEFAULT_RULE_COUNT = 2
+
+  def self.convert(recurrence)
+    if recurrence.is_a? IceCube::Rule
+      schedule = IceCube::Schedule.new
+      schedule.add_recurrence_rule recurrence.count(DEFAULT_RULE_COUNT)
+      occurrence = schedule.all_occurrences
+      time_in_seconds = occurrence.last.to_time.to_i - occurrence.first.to_time.to_i
+      time_in_seconds
+    else
+      recurrence
+    end
+  end
+
+end
+
+describe 'IceCube To Whenever cron syntax' do
+
+  let(:cron_result) { "0 0 1,8,15,22 * *" }
+  let(:at_time) { nil }
+  let(:time_in_cron) { Whenever::Output::Cron.new(time, nil, at_time).time_in_cron_syntax }
+
+  describe ActiveSupport::Duration do
+    let(:time) { 1.week }
+
+    it 'converts regular time to cron syntax' do
+      expect(time_in_cron).to eq cron_result
+    end
+  end
+
+  describe :time_in_seconds do
+    let(:time) { 1.week.to_i }
+
+    it 'converts time in seconds to cron syntax' do
+      expect(time_in_cron).to eq cron_result
+    end
+  end
+
+  describe :ice_cube do
+
+    let(:time) { TimeConversions.convert(time_rule) }
+
+    describe :daily do
+      let(:cron_result) { "0 0 * * *" }
+      let(:time_rule) { IceCube::Rule.daily }
+
+      it 'converts ice_cube time to cron syntax' do
+        expect(time_in_cron).to eq cron_result
+      end
+    end
+
+    describe :weekly do
+      let(:cron_result) { "0 0 1,8,15,22 * *" }
+      let(:time_rule) { IceCube::Rule.weekly }
+
+      it 'converts ice_cube time to cron syntax' do
+        expect(time_in_cron).to eq cron_result
+      end
+    end
+
+    describe :monthly do
+      let(:cron_result) { "0 0 1 * *" }
+      let(:time_rule) { IceCube::Rule.monthly }
+
+      it 'converts ice_cube time to cron syntax' do
+        expect(time_in_cron).to eq cron_result
+      end
+    end
+
+    describe :date_with_time do
+
+      let(:at_time) { '12:20am' }
+      let(:cron_result) { "20 0 1 * *" }
+      let(:time_rule) { IceCube::Rule.monthly }
+
+      it 'converts ice_cube time to cron syntax' do
+        expect(time_in_cron).to eq cron_result
+      end
+    end
+
+  end
+
+end
